b783fee4e6f541268b10f41778c703cb9af07439
TigioAguirre <166470888+TigioAguirre@users.noreply.github.com>
2025-05-21 00:22:29 -0500

    Update semana8Taller.c

--- semana8Taller.c
+++ semana8Taller.c
@@ -1,7 +1,6 @@
 //TALLER SEMANA 8
 //AGUIRRE REMIGIO-MACÍAS ISSAC-HIDALGO MATEO
 #include "libS8.h"
-
 void main(){
     struct Recursos recurso[maxRecursos];
     struct Productos producto[maxProductos];
@@ -12,6 +11,7 @@ A:
     printf("|Ingrese el numero de recursos existentes(Maximo %d):\n",maxRecursos);
     printf("|");
     scanf("%d",&nR);
+    getchar();//LIMPIAR EL SALTO DE LINEA /n que se genera al dar enter para el fget
         if (nR>0&&nR<=maxRecursos){
         }else{
             printf("-----------------------------------------------------\n");
@@ -21,10 +21,18 @@ A:
     for (int i = 0; i < nR; i++) {
         printf("-----------------------------------------------------\n");
         printf("| Nombre del Recurso #%d: ",i+1);
-        scanf("%s", recurso[i].nombre);
+        fgets(recurso[i].nombre, 50, stdin);
+        limpiarSaltoLinea(recurso[i].nombre);    
+        //scanf("%s",recurso[i].nombre );
+        printf("| Ingrese la unidad de medida de cantidad para 1 %s: ",recurso[i].nombre);
+        fgets(recurso[i].magnitud, 50, stdin);
+        limpiarSaltoLinea(recurso[i].magnitud);    
+        //scanf("%s", recurso[i].magnitud);
+        
     B:
         printf("| Cantidad inicial de %s: ",recurso[i].nombre);
         scanf("%f", &recurso[i].cantidad);
+        getchar();//LIMPIAR EL SALTO DE LINEA /n que se genera al dar enter para el fget
             if (recurso[i].cantidad>=0){
             }else{
                 printCantRecursosInvalid();
@@ -38,11 +46,14 @@ A:
         for (int i = 0; i < maxProductos; i++) {
             printf("-----------------------------------------------------\n");
             printf("| Nombre del Producto #%d: ",i+1);
-            scanf("%s", producto[i].nombre);  
+            fgets(producto[i].nombre, 50, stdin);
+            limpiarSaltoLinea(producto[i].nombre);   
+            //scanf("%s", producto[i].nombre);  
         C:
             printf("| Ingrese la cantidad de Recursos que %s necesita\n",producto[i].nombre);
             printf("| MAXIMO %d Recursos: ",nR);
             scanf("%d",&cR);
+            getchar();//LIMPIAR EL SALTO DE LINEA /n que se genera al dar enter para el fget
                 if (cR>0&&cR<=nR){
                     producto[i].cantRecursos=cR;
                 }else{     
@@ -53,22 +64,25 @@ A:
                     printf("-----------------------------------------------------\n");
                     printf("| Id del recurso #%d de %s: ",j+1,producto[i].nombre);
                     scanf("%d", &producto[i].caR[j].id);  
+                    getchar();//LIMPIAR EL SALTO DE LINEA /n que se genera al dar enter para el fget
                         if ((buscarRecursoPorID(recurso,maxRecursos,producto[i].caR[j].id))!=-1){
                         }else{
                             printidRecursosInvalido();
                             goto D;};
                 E:
-                    printf("| Cantidad necesaria de %s: ",recurso[buscarRecursoPorID(recurso,maxRecursos,producto[i].caR[j].id)].nombre);
+                    printf("| Cantidad necesaria en %s de %s: ",recurso[buscarRecursoPorID(recurso,maxRecursos,producto[i].caR[j].id)].magnitud,recurso[buscarRecursoPorID(recurso,maxRecursos,producto[i].caR[j].id)].nombre);
                     scanf("%f", &producto[i].caR[j].cantidad);
+                    getchar();//LIMPIAR EL SALTO DE LINEA /n que se genera al dar enter para el fget
                         if (producto[i].caR[j].cantidad>0){
                         }else{
                             printCantRecursosInvalid();
                             goto E;}
-                        printf("%s necesita %.2f de %s\n", producto[i].nombre,producto[i].caR[j].cantidad,recurso[buscarRecursoPorID(recurso,maxRecursos,producto[i].caR[j].id)].nombre);
+                        printf("%s necesita %.2f %s de %s\n", producto[i].nombre,producto[i].caR[j].cantidad,recurso[buscarRecursoPorID(recurso,maxRecursos,producto[i].caR[j].id)].magnitud,recurso[buscarRecursoPorID(recurso,maxRecursos,producto[i].caR[j].id)].nombre);
                     }
             printf("-----------------------------------------------------\n");
             printf("| Tiempo de fabricacion en minutos para 1 %s: ",producto[i].nombre);
             scanf("%f",&producto[i].mP);
+            getchar();//LIMPIAR EL SALTO DE LINEA /n que se genera al dar enter para el fget
             producto[i].id = idRandom();
             printf("\n============== DETALLES DEL PRODUCTO %d ==============\n",i+1);
             printf("Nombre del Producto: %s\n", producto[i].nombre);
@@ -78,7 +92,7 @@ A:
                 for (int j = 0; j < cR; j++) {
                     int pos = buscarRecursoPorID(recurso, nR, producto[i].caR[j].id);
                         if (pos != -1) {
-                            printf("Recurso %s (ID: %d), Cantidad requerida: %.2f\n",recurso[pos].nombre,producto[i].caR[j].id,producto[i].caR[j].cantidad);}
+                            printf("Recurso %s (ID: %d), Cantidad requerida: %.2f %s\n",recurso[pos].nombre,producto[i].caR[j].id,producto[i].caR[j].cantidad,recurso[pos].magnitud);}
                     }
             printf("=====================================================\n");
         }
@@ -87,6 +101,7 @@ A:
     F:
     printMenuP();
     scanf("%d",&opcion);
+    getchar();//LIMPIAR EL SALTO DE LINEA /n que se genera al dar enter para el fget
     switch (opcion){
                 case 1:
                     int i,id;
@@ -95,14 +110,16 @@ A:
                 G:
                     printf("| Ingrese el ID del producto : \n|" );
                     scanf("%d",&id);
+                    getchar();//LIMPIAR EL SALTO DE LINEA /n que se genera al dar enter para el fget
                         if (buscarProductoPorID(producto,maxProductos,id)!=-1){
                             i=buscarProductoPorID(producto,maxProductos,id);
                         }else{
                             printIdProductoInvalid();
                             goto G;}
                 H:
-                    printf("| Ingrese la cantidad requerida de %s : \n",producto[i].nombre );
+                    printf("| Ingrese la cantidad requerida por el cliente de %s : \n",producto[i].nombre );
                     scanf("%f",&cantRequerida);
+                    getchar();//LIMPIAR EL SALTO DE LINEA /n que se genera al dar enter para el fget
                         if (cantRequerida>0){
                         }else{
                             printf("-----------------------------------------------------\n");
@@ -112,6 +129,7 @@ A:
                 I:
                     printf("| Ingrese el tiempo (minutos) solicitado para la entrega de %.2f %s : \n",cantRequerida,producto[i].nombre );//recurso[i].nombre, recurso[i].id
                     scanf("%f",&tiemRequerido);
+                    getchar();//LIMPIAR EL SALTO DE LINEA /n que se genera al dar enter para el fget
                         if (tiemRequerido>0){
                         }else{
                             printf("-----------------------------------------------------\n");
@@ -131,7 +149,7 @@ A:
                     for (int j = 0; j < (producto[i].cantRecursos); j++) {
                         int pos = buscarRecursoPorID(recurso, nR, producto[i].caR[j].id);
                             if (pos != -1) {
-                                printf("| Recurso %s (ID: %d), Cantidad requerida: %.2f\n| Cantidad actual de %s: %.2f\n",recurso[pos].nombre,producto[i].caR[j].id,producto[i].caR[j].cantidad*cantRequerida,recurso[pos].nombre,recurso[pos].cantidad);
+                                printf("| Recurso %s (ID: %d), Cantidad requerida: %.2f %s\n| Cantidad actual de %s: %.2f %s\n",recurso[pos].nombre,producto[i].caR[j].id,producto[i].caR[j].cantidad*cantRequerida,recurso[pos].magnitud,recurso[pos].nombre,recurso[pos].cantidad,recurso[pos].magnitud);
                             }
                             if (producto[i].caR[j].cantidad*cantRequerida<=recurso[pos].cantidad){
                                 printf("|Existencia de recursos:\n| SI cumple con los recursos necesitados\n");    
@@ -151,6 +169,7 @@ A:
                 J:
                     printf("| Ingrese el ID del producto a Editar : \n|" );
                     scanf("%d",&ld);
+                    getchar();//LIMPIAR EL SALTO DE LINEA /n que se genera al dar enter para el fget
                         if (buscarProductoPorID(producto,maxProductos,ld)!=-1){
                             l=buscarProductoPorID(producto,maxProductos,ld);
                         }else{
@@ -162,15 +181,18 @@ A:
                     printf("-----------------------------------------------------\n");
                     printf("|1. EDITAR NOMBRE\n");
                     printf("|2. EDITAR MATERIALES REQUERIDOS PARA FABRICAR 1 %s\n",producto[l].nombre);
-                    printf("|3. EDITAR TIEMPO DE FABRICACION DE 1%s\n",producto[l].nombre);
+                    printf("|3. EDITAR TIEMPO DE FABRICACION DE 1 %s\n",producto[l].nombre);
                     printf("|4. SALIR\n");
                     printf("-----------------------------------------------------\n|");
                     scanf("%d",&opC2);
+                    getchar();//LIMPIAR EL SALTO DE LINEA /n que se genera al dar enter para el fget
                         switch (opC2){
                                     case 1:
                                         printf("-----------------------------------------------------\n");
                                         printf("| Nuevo nombre del Producto #%d: ",l+1);
-                                        scanf("%s", producto[l].nombre); 
+                                        fgets(producto[l].nombre, 50, stdin);
+                                        limpiarSaltoLinea(producto[l].nombre);    
+                                        //scanf("%s", producto[l].nombre); 
                                     goto K;
 
                                     case 2:
@@ -178,6 +200,7 @@ A:
                                         printf("| Ingrese la cantidad de Recursos que %s necesita\n",producto[l].nombre);
                                         printf("| MAXIMO %d Recursos: ",nR);
                                         scanf("%d",&cR);
+                                        getchar();//LIMPIAR EL SALTO DE LINEA /n que se genera al dar enter para el fget
                                             if (cR>0&&cR<=nR){
                                                 producto[i].cantRecursos=cR;
                                             }else{     
@@ -188,18 +211,20 @@ A:
                                                 printf("-----------------------------------------------------\n");
                                                 printf("| Id del recurso #%d de %s: ",j+1,producto[l].nombre);
                                                 scanf("%d", &producto[l].caR[j].id);  
+                                                getchar();//LIMPIAR EL SALTO DE LINEA /n que se genera al dar enter para el fget
                                                     if ((buscarRecursoPorID(recurso,maxRecursos,producto[l].caR[j].id))!=-1){
                                                     }else{
                                                         printidRecursosInvalido();
                                                         goto M;};
                                             N:
-                                                printf("| Cantidad necesaria de %s: ",recurso[buscarRecursoPorID(recurso,maxRecursos,producto[l].caR[j].id)].nombre);
+                                                printf("| Cantidad necesaria de %s en %s: ",recurso[buscarRecursoPorID(recurso,maxRecursos,producto[l].caR[j].id)].nombre,recurso[buscarRecursoPorID(recurso,maxRecursos,producto[l].caR[j].id)].magnitud);
                                                 scanf("%f", &producto[l].caR[j].cantidad);
+                                                getchar();//LIMPIAR EL SALTO DE LINEA /n que se genera al dar enter para el fget
                                                     if (producto[l].caR[j].cantidad>0){
                                                     }else{
                                                         printCantRecursosInvalid();  
                                                         goto N;}
-                                                printf("%s necesita %.2f de %s\n", producto[l].nombre,producto[l].caR[j].cantidad,recurso[buscarRecursoPorID(recurso,maxRecursos,producto[l].caR[j].id)].nombre);
+                                                printf("%s necesita %.2f %s de %s\n", producto[l].nombre,producto[l].caR[j].cantidad,recurso[buscarRecursoPorID(recurso,maxRecursos,producto[l].caR[j].id)].magnitud,recurso[buscarRecursoPorID(recurso,maxRecursos,producto[l].caR[j].id)].nombre);
                                             }
                                         printf("-----------------------------------------------------\n");
                                     goto K;
@@ -208,6 +233,7 @@ A:
                                         printf("-----------------------------------------------------\n");
                                         printf("| Nuevo tiempo de Fabricacion en minutos para 1 %s: ",producto[l].nombre);
                                         scanf("%f", &producto[l].mP); 
+                                        getchar();//LIMPIAR EL SALTO DE LINEA /n que se genera al dar enter para el fget
                                     goto K;
 
                                     case 4:
@@ -222,16 +248,18 @@ A:
                 case 3:
                     printf("| Ingrese el ID del producto a eliminar:\n| ");
                     scanf("%d", &ld);
+                    getchar();//LIMPIAR EL SALTO DE LINEA /n que se genera al dar enter para el fget
                     l = buscarProductoPorID(producto, maxProductos, ld);
                         if (l != -1) {
                             char confirmacion[5];
-                            printf("| Para confirmar la eliminación del producto escriba 'SI', para cancelar 'NO':\n| ");
+                            printf("| Para confirmar la eliminaciOn del producto escriba 'SI':\n| ");
                             scanf("%s", confirmacion);
+                            getchar();//LIMPIAR EL SALTO DE LINEA /n que se genera al dar enter para el fget
                                 if (strcmp(confirmacion, "SI") == 0) {
                                     producto[l].id = -1; 
                                     printf("| Producto eliminado correctamente.\n");
                                 } else {
-                                    printf("| Eliminación cancelada.\n");}
+                                    printf("| Eliminacion cancelada.\n");}
                         } else {
                             printf("| ID de producto no encontrado.\n");}
                 goto F;
